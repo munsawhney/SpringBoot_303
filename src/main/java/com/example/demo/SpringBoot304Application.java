5d4dfdf64d92ed1148788a5392efa7b47360926a
kellel14 <kellel14@gmail.com>
2019-07-24 20:40:57 -0400

    Auto Add Records to Database

--- src/main/java/com/example/demo/SpringBoot304Application.java
+++ src/main/java/com/example/demo/SpringBoot304Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot303Application {
+public class SpringBoot304Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot303Application.class, args);
+		SpringApplication.run(SpringBoot304Application.class, args);
 	}
 
 }
